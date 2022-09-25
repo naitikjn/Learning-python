676b2c26314773de51a49b6468c0ccfa8772287f
naitikjn <trendytech29@gmail.com>
2022-09-25 18:37:22 +0530

    25/09/2022 sunday Today's work

--- Python tuts.py
+++ Python tuts.py
@@ -9,5 +9,9 @@ print("bhai like bhi kar diya karo")
 print("next line")
 print("C:\naitik")
 print("C:\Riya")
+<<<<<<< HEAD
 print("Hello world",end= "pagal riya")
+=======
+print("Hello world",end= " ")
+>>>>>>> bb7d7a0 (25/09/2022 sunday)
 print("next line")
