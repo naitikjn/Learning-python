8142595be8e76a9cd0d4d9605e9454a5decc258d
naitikjn <trendytech29@gmail.com>
2022-09-25 18:37:22 +0530

    25/09/2022 sunday Today's work

---
+++ Python tuts.py
@@ -0,0 +1,13 @@
+#please ignore this one  -  SINGLE LINE COMMENT
+"""
+Hello
+brothers
+and sisters   - MULTI LINE COMMENT
+"""
+print("subscribe now", end=" joker ")
+print("bhai like bhi kar diya karo")
+print("next line")
+print("C:\naitik")
+print("C:\Riya")
+print("Hello world",end= "pagal riya")
+print("next line")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 def main():
-    print("Hello World!")
+    print("Hello ")
+
 
 
 if __name__ == '__main__':
