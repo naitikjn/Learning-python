aadfe9fbf87980c6d7965119085322f20a3cd27a
naitikjn <trendytech29@gmail.com>
2022-09-26 19:28:37 +0530

    26/09/2022 monday Today's work

--- Python tuts.py
+++ Python tuts.py
@@ -1,17 +1,16 @@
-#please ignore this one  -  SINGLE LINE COMMENT
+#please ignore this one                      # SINGLE LINE COMMENT
 """
 Hello
 brothers
-and sisters   - MULTI LINE COMMENT
+and sisters   -                              # MULTI LINE COMMENT
 """
-print("subscribe now", end=" joker ")
-print("bhai like bhi kar diya karo")
-print("next line")
-print("C:\naitik")
-print("C:\Riya")
-<<<<<<< HEAD
-print("Hello world",end= "pagal riya")
-=======
+#print("subscribe now", end=" joker ")
+#print("bhai like bhi kar diya karo")
+#print("next line")
+#print("C:\naitik")
+
 print("Hello world",end= " ")
->>>>>>> bb7d7a0 (25/09/2022 sunday)
 print("next line")
+print("C:\\naitik")
+print("naitik \n is \n good \n boy \t  ")      # Escape sequence
+print("miss hill \b school")
