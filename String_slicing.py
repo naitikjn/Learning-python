a46e308e8e5e46e4d35dfeea7480b747bdb266cb
naitikjn <trendytech29@gmail.com>
2022-09-27 18:54:35 +0530

    26/09/2022 monday Today's work

---
+++ String_slicing.py
@@ -0,0 +1,12 @@
+mystr="Naitik is a good boy"
+print(mystr)
+print(len(mystr))            # length of String
+print(mystr[-8:-5])          # Indexing in String
+print(mystr[-10])
+abc = 'Python is a programming language'
+print(abc.find("is"))                                  # String methods and its functions
+print(abc.endswith("language"))
+print(abc.count("p"))
+print(abc.capitalize())
+print(abc.upper())
+print(abc.replace("programming" , "Python"))
