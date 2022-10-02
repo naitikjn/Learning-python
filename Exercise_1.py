34d0b2ece4ff1a95471e2ec40d3d7a2c2d83b1cf
naitikjn <trendytech29@gmail.com>
2022-10-02 21:35:48 +0530

    Merge remote-tracking branch 'github/master'

---
+++ Exercise_1.py
@@ -0,0 +1,20 @@
+# Create a dictionary and take a input from the user and return the meaning of the word from dictinary
+#1
+House = {"Abhimanyu singh": "Green",
+         "Divyansh tripathi": "Yellow",
+         "Lokendra shakya": "Red",
+         "Naitik jain": "Blue"
+         }
+name = input("Enter name: ")
+print(House[name])
+
+#2
+Subjectstream ={"Abhimanyu singh": "PCM " +"+ Physical education",
+                 "Divyansh tripathi":"PCM " +"+ Physical education",
+                 "Naitik jain": "Commerce "+ "+ Computer science",
+                 "Lokendra shakya": "Humanties "+ "+ Hindi"
+                  }
+
+name2 = input("Enter name: ")
+print(Subjectstream[name2])
+print("ok Thank you")
