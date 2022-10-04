b2252b88cb3df20aa59692646daba5f9a2a17062
naitikjn <trendytech29@gmail.com>
2022-10-04 21:26:15 +0530

    26/09/2022 monday Today's work

---
+++ if_Else_Elif.py
@@ -0,0 +1,15 @@
+var1 = 6
+var2 =56
+
+var3 =int(input())
+
+if var3>var2:
+    print("Greator")
+
+else:
+    print("Lesser")
+     print(d2["Rohit"])
+ print(d2["Rohit"]["B"])
+     del d2["Ram"]
+     d3 = d2.copy()
+     print(d2.get("Kohli"))
