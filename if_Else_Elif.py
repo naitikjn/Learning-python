03cd6a0234f48cb035ecd46abe5c2400c28688be
naitikjn <trendytech29@gmail.com>
2022-10-04 21:26:15 +0530

    26/09/2022 monday Today's work

--- if_Else_Elif.py
+++ if_Else_Elif.py
@@ -6,6 +6,7 @@ var3 =int(input())
 if var3>var2:
     print("Greator")
 
+<<<<<<< HEAD
 else:
     print("Lesser")
      print(d2["Rohit"])
@@ -13,3 +14,27 @@ else:
      del d2["Ram"]
      d3 = d2.copy()
      print(d2.get("Kohli"))
+=======
+if var3==var2:
+    print("Equal")
+
+else:
+    print("Lesser")
+
+
+list1 = [1,2,5,9]
+print(15 not in list1)
+if 15 in list1:
+    print("Yes it is in the list")
+
+print("What is your age")
+age = int(input())
+if age<18:
+    print("You cannot drive")
+
+elif age ==18:
+    print("We will think about you")
+
+else:
+    print("You can drive")
+>>>>>>> fec2849 (26/09/2022 monday)
