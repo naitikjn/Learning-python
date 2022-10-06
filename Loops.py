5778ce12084deb46021a0b4d3b58bf75e603a6ab
naitikjn <trendytech29@gmail.com>
2022-10-06 18:42:27 +0530

    26/09/2022 monday Today's work

---
+++ Loops.py
@@ -0,0 +1,13 @@
+list1 = ['harry', 1],['larry', 2],['karan', 3],['prakash', 4]
+#print(list1[0],list1[1])
+for item , lollypop in list1:
+    print(item,"and lolly is", lollypop)
+    var1 = "Hello "
+    var2 = "Trendy tech"
+
+print(str(var1) + str(var2))
+print(100* "hello world \n")
+print("enter your number")
+abcd = input()
+
+print("you entered", int(abcd) + 1)
