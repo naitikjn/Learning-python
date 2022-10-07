8d5d551b530a8bfd4a1913e85805cb08fa2897e9
naitikjn <trendytech29@gmail.com>
2022-10-07 19:59:12 +0530

    26/09/2022 monday Today's work

--- Loops.py
+++ Loops.py
@@ -2,12 +2,35 @@ list1 = ['harry', 1],['larry', 2],['karan', 3],['prakash', 4]
 #print(list1[0],list1[1])
 for item , lollypop in list1:
     print(item,"and lolly is", lollypop)
-    var1 = "Hello "
-    var2 = "Trendy tech"
 
+
+    var1 = "Hell" \
+           "o "
+    var2 = "Trendy tech"
+#types python
 print(str(var1) + str(var2))
 print(100* "hello world \n")
 print("enter your number")
 abcd = input()
 
 print("you entered", int(abcd) + 1)
+
+# While loops in python
+i = 0
+while(i<45):
+    print(i)
+
+    i = i+1
+
+i = 10
+while(i<50):
+    print(i)
+    i =i +9
+
+i = 1
+while(i<50):
+    i =i +1
+
+list1 = ['harry', 1], ['larry', 2], ['smith', 3]
+for item , lollypop in list1:
+    print(item,"and mohan is", lollypop)
