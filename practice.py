6cd2d5049d60cb6b41caf9830537182a7a634036
naitikjn <trendytech29@gmail.com>
2022-10-09 17:22:43 +0530

    26/09/2022 monday Today's work

---
+++ practice.py
@@ -0,0 +1,23 @@
+var1 = "Hello "
+var2 = "Inspiron 3000"
+var3 = 56
+var4 = 14
+
+print(str(var1) + str(var2))
+print(100* "hello world \n")
+
+
+Numbers = [2,5,4,9,7]
+Numbers.sort()
+Numbers.reverse()
+print(max(Numbers))
+print(min(Numbers))
+Numbers.append(5)
+print(Numbers)
+
+
+print("Hello world",end= " ")
+print("next line")
+print("C:\\naitik")
+print("naitik \n is \n good \n boy \t  ")      # Escape sequence
+print("miss hill \b school")
