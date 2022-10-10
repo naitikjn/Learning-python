5efd249b662678c076c15e1ad82339d13145a2d1
naitikjn <trendytech29@gmail.com>
2022-10-10 21:35:38 +0530

    26/09/2022 monday Today's work

--- practice.py
+++ practice.py
@@ -21,3 +21,11 @@ print("next line")
 print("C:\\naitik")
 print("naitik \n is \n good \n boy \t  ")      # Escape sequence
 print("miss hill \b school")
+
+abc = 'Python is a programming language'
+print(abc.find("is"))                                  # String methods and its functions
+print(abc.endswith("language"))
+print(abc.count("p"))
+print(abc.capitalize())
+print(abc.upper())
+print(abc.replace("programming" , "Python"))
