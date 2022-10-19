378b09154866fcbb016ce572cbc762c7e8bd47aa
naitikjn <trendytech29@gmail.com>
2022-10-19 22:02:02 +0530

    26/09/2022 monday Today's work

---
+++ Break_and_continue.py
@@ -0,0 +1,15 @@
+i = 0
+
+while(True) :
+    print(i+1, end= "  ")
+    i = i + 5
+
+    i = 0
+    if i+1<5:
+        i = i + 1
+        continue
+        print(i + 1, end="  ")
+        if (i==44):
+
+            break
+            i = i + 1
