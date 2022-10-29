a0244cf9046969e5e3451e2639fc3b94d6d6e146
naitikjn <trendytech29@gmail.com>
2022-10-29 19:37:29 +0530

    Merge remote-tracking branch 'github/master'

---
+++ operators.py
@@ -0,0 +1,31 @@
+#Operators in python
+#Arithmetic operator
+#Assignment operator
+#Comparison operator
+#Logical operator
+#Identity operator
+#Membership operator
+#Bitwise operator
+
+
+print("5+6 is ",5+6)
+print("5-6 is ", 5-6)
+print("5*6 is ",5*6)
+print("5/6 is ",5/6)
+print("5%6 is ",5%6)
+print("5**6 is ",5**6)
+print("5//6 is ",5//6)
+
+#Assignment operator
+print("Assignment operator")
+x = 5
+x += 7
+print(x)
+
+y = 8
+y *= 5
+print(y)
+
+z = 10
+z /= 5
+print(z)
