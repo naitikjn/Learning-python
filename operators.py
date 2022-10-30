db621b9d6468f6a9c280bbd223e1acea40f9a3c7
naitikjn <trendytech29@gmail.com>
2022-10-30 10:37:29 +0530

    30/10/2022 sunday

--- operators.py
+++ operators.py
@@ -35,6 +35,13 @@ z_1 =- 5
 print(z_1)
 
 
+#Logical operator
+a = True
+b = False
+b = 8
+print(b and 8)
+print(b or 5)
+
 #Comparison operator
 a = 8
 print(a==8)
@@ -42,8 +49,27 @@ print(a<=5)
 print(a>=5)
 
 
+#Identity operator
+a = True
+b = False
+print(5 is not 5)
+print(5 or 8)
+print(5 and 5)
 
-#Logical operator
-b = 8
-print(b and 8)
-print(b or 5)
+#Membership operator
+List = [5 , 55 , 65 , 48 , 14 , 25 , 47 , 65 , 32 , 24 , 15 , 20 , 10 , 85]
+print(32 or 14 in List)
+print(85 and 20 in List)
+print(48 is not List)
+
+#Bitwise operator
+#0 - 00
+#1 - 01
+#2 - 10
+#3 - 11
+print(0 & 1)
+print(0 | 1)
+print(0 | 3)
+print(0 & 2)
+
+print("exit code")
