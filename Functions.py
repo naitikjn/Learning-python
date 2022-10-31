c64b705adadef877e45428146080695873dd559e
naitikjn <trendytech29@gmail.com>
2022-10-31 19:27:17 +0530

    31/10/2022 monday

---
+++ Functions.py
@@ -0,0 +1,22 @@
+from copy import d
+
+a = 9
+b = 8
+c = sum((a,b))
+print(c)
+
+def function1(a,b):
+    print("Hello you are in function1",a+b)
+
+function1()
+function1()
+function1()
+function1()
+
+
+def function2(a, b):
+    average =  (a+b)/2
+    print(average)
+function2()
+
+
