95e3d348fc4b38f26d96951991890c14ee184788
naitikjn <trendytech29@gmail.com>
2022-11-09 21:41:57 +0530

    09/11/2022 - Wednesday Today's work

--- practice.py
+++ practice.py
@@ -11,3 +11,25 @@ elif var3==var2:
 
 else:
    print("lesser")
+#sets
+sst = (1,2,3,4)
+print(s_from_ = set()
+print(type(s))
+s_from_lilist)
+print(type(s_from_list))
+l = [1,3,5,7,9]
+s = set(l)
+print(s)
+s.add(8)
+s.add(8)
+print(s)
+s.remove(8)
+print(s)
+
+#types python
+print(str(var1) + str(var2))
+print(100* "hello world \n")
+print("enter your number")
+abcd = input()
+
+print("you entered", int(abcd) + 1)
