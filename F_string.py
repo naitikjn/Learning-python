b319a3fae5c2180c23a0159f93aaf65aa22271ee
naitikjn <trendytech29@gmail.com>
2022-11-22 19:05:19 +0530

    hello

---
+++ F_string.py
@@ -0,0 +1,28 @@
+# name="Jack”
+#
+#  n="%s My name is %s” %name
+#
+# print(n)
+#
+#  Output: "My name is Jack."
+# name=”Jack”
+# class=5
+# s=”%s is in class %d”%(name,class)
+# print(s)str = "This article is written in {} "
+#
+# print (str.format("Python"))
+# # F strings
+# import math
+#
+# me = "Harry"
+# a1 =3
+# # a = "this is %s %s"%(me, a1)
+# # a = "This is {1} {0}"
+# # b = a.format(me, a1)
+# # print(b)
+# a = f"this is {me} {a1} {math.cos(65)}"
+# # time
+# print(a)
+#
+#
+
