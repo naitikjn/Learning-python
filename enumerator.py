481ca71ef98e87cc209d49281769268f8bf957e1
naitikjn <trendytech29@gmail.com>
2022-11-26 21:20:01 +0530

    hello

---
+++ enumerator.py
@@ -0,0 +1,14 @@
+l1 = ["Bhindi", "Aloo", "chopsticks", "chowmein"]
+
+# i = 1
+# for item in l1:
+#     if i%2 is not 0:
+#         print(f"Jarvis please buy {item}")
+#     i += 1
+
+for index, item in enumerate(l1):
+    if index % 2 == 0:
+        print(f"Jarvis please buy {item}")
+
+
+
