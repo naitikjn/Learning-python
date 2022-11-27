5015232998bf568e8a5d33cc2ec8f0b4f3c24e4a
naitikjn <trendytech29@gmail.com>
2022-11-27 19:40:25 +0530

    hello

--- practice.py
+++ practice.py
@@ -1,35 +1,62 @@
-else_if_elif
-var1 = 8
-var2 = 45
+# else_if_elif
+# var1 = 8
+# var2 = 45
+#
+# var3 = int(input())
+# if var3>var2:
+#     print("greater")
+#
+# elif var3==var2:
+#     print("equal")
+#
+# else:
+#    print("lesser")
+# #sets
+# sst = (1,2,3,4)
+# print(s_from_ = set()
+# print(type(s))
+# s_from_lilist)
+# print(type(s_from_list))
+# l = [1,3,5,7,9]
+# s = set(l)
+# print(s)
+# s.add(8)
+# s.add(5)
+# print(s)
+# s.remove(8)
+# print(s)
+#
+# #types python
+# print(str(var1) + str(var2))
+# print(100* "hello world \n")
+# print("enter your number")
+# abcd = input()
+#
+# print("you entered", int(abcd) + 1)
+#
+# import sys
+# print( sys.path)
+#
+# # There are two methods to use functions or variables after importing:
+# # The first one is to import using an object. For this, we usually import the whole module by using a simple import statement. When we use only the import keyword, we will import the resource directly, like this:
+# # import sklearn
+# # When we use the second syntax, we will import the resource from another package or module. Here is an example:
+# # from flask import Flask
+# # We can also choose to rename an imported resource, like this:
+# # import pandas as pd
+# from sklearn.ensemble import RandomForestClassifier
+#
+# print(RandomForestClassifier())
+#
+# import file2
+#
+# print(file2.a)
+#
+# file2.printjoke("This is me")
+# a = 7
+#
+#
+# def printjoke(str):
+#     print(f"this function is a joke {str}")
 
-var3 = int(input())
-if var3>var2:
-    print("greater")
 
-elif var3==var2:
-    print("equal")
-
-else:
-   print("lesser")
-#sets
-sst = (1,2,3,4)
-print(s_from_ = set()
-print(type(s))
-s_from_lilist)
-print(type(s_from_list))
-l = [1,3,5,7,9]
-s = set(l)
-print(s)
-s.add(8)
-s.add(5)
-print(s)
-s.remove(8)
-print(s)
-
-#types python
-print(str(var1) + str(var2))
-print(100* "hello world \n")
-print("enter your number")
-abcd = input()
-
-print("you entered", int(abcd) + 1)
