7bd4d423ef2595fdbc49bee36fcbc54d6abe6595
naitikjn <trendytech29@gmail.com>
2022-12-07 21:40:59 +0530

    hello

--- practice.py
+++ practice.py
@@ -293,6 +293,48 @@ print(o)
 # o = "num1"
 # o = int(input("Enter the number: "))
 # p = 'num2'
+
 # p = int(input("Enter the number: "))
 # print(o+p)
 #
+
+# def function1():
+#     print("Subscribe now")
+#
+# func2 = function1
+# del function1
+# func2()
+
+# def funcret(num):
+#     if num==0:
+#         return print
+#     if num==1:
+#         return sum
+#
+# a = funcret(1)
+# print(a)
+
+# def executor(func):
+#     func("this")
+#
+#
+# executor(print)
+
+def dec1(func1):
+    def nowexec():
+        print("Executing now")
+        func1()
+        print("Executed")
+
+    return nowexec
+
+
+@dec1
+def who_is_harry():
+    print("Harry is a good boy")
+
+
+# who_is_harry = dec1(who_is_harry)
+
+who_is_harry()
+
