86054a48a67e714bf41033e0aa35ef8b6f1e97df
naitikjn <trendytech29@gmail.com>
2022-12-10 21:51:49 +0530

    hello

--- practice.py
+++ practice.py
@@ -470,3 +470,29 @@ if noofchance==chance:
     print("GAME OVER ^_~")
     if noofchance==10:
         quit()
+
+        list1 = ['harry', 1], ['larry', 2], ['karan', 3], ['prakash', 4]
+        # print(list1[0],list1[1])
+        for item, lollypop in list1:
+            print(item, "and lolly is", lollypop)
+
+            var1 = "Hell" \
+                   "o "
+            var2 = "Trendy tech"
+        # types python
+        print(str(var1) + str(var2))
+        print(100 * "hello world \n")
+        print("enter your number")
+        abcd = input()
+
+        print("you entered", int(abcd) + 1)
+
+        # While loops in python
+        i = 0
+        while (i < 45):
+            print(i)
+
+            i = i + 1
+
+        i = 10
+        while (i < 50):
