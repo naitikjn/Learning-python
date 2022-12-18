74ff7b49d45b5580abf88b06df67fd9c47a19862
naitikjn <trendytech29@gmail.com>
2022-12-18 09:41:49 +0530

    hello

--- Data types.py
+++ Data types.py
@@ -1,27 +1,32 @@
-var1 = "Hello "
-var2 = "Trendy tech"
-var3 = 56
-var4 = 14
+# var1 = "Hello "
+# var2 = "Trendy tech"
+# var3 = 56
+# var4 = 14
+#
+# print(str(var1) + str(var2))
+# print(100* "hello world \n")
+# print("enter your number")
+# abcd = input()
+#
+# print("you entered", int(abcd) + 10)
+#
+# print("Enter the number")
+# n1 = input()
+# print("Enter the number")
+# n2 = input()
+# print("Sum of these numbers: ", int(n1) + int(n2))
+#
+# # Data types
+# """
+# 1. int()
+# 2. float()
+# 3. str()
 
-print(str(var1) + str(var2))
-print(100* "hello world \n")
-print("enter your number")
-abcd = input()
-
-print("you entered", int(abcd) + 10)
-
-print("Enter the number")
-n1 = input()
-print("Enter the number")
-n2 = input()
-print("Sum of these numbers: ", int(n1) + int(n2))
-
-# Data types
-"""
-1. int()
-2. float()
-3. str()
-"""
+# variables & data types
+a =1
+print(a)
+b = 23
+print(a+b)
 
 
 
--- practice.py
+++ practice.py
@@ -838,3 +838,45 @@ if __name__ == "__main__":
                 speak("Sorry my friend harry bhai. I am not able to send this email")
         else:
             print("No query matched"
+
+
+class Student:
+    pass
+
+
+harry = Student()
+larry = Student()
+
+harry.name = "Harry"
+harry.std = 12
+harry.section = 1
+larry.std = 9
+larry.subjects = ["hindi", "physics"]
+print(harry.section, larry.subjects)
+
+
+class Employee:
+    no_of_leaves = 8
+    pass
+
+harry = Employee()
+rohan = Employee()
+
+harry.name = "Harry"
+harry.salary = 455
+harry.role = "Instructor"
+
+rohan.name = "Rohan"
+rohan.salary = 4554
+rohan.role = "Student"
+
+print(Employee.no_of_leaves)
+print(Employee.__dict__)
+Employee.no_of_leaves = 9
+print(Employee.__dict__)
+print(Employee.no_of_leaves)
+
+
+
+
+
