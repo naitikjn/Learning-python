7432d4ed8c86c9e58065aa9f2b09b6e02c7ae857
naitikjn <trendytech29@gmail.com>
2022-12-19 19:41:22 +0530

    hello

--- practice.py
+++ practice.py
@@ -893,3 +893,22 @@ else:
     print("you are not eligible for vote,wait for some time")
 
 
+# If __name__==__main__ usage & necessity
+if __name__ == '__main__':
+  def printhar(str) :
+    str = "sahab"
+    return f"yeh string mujhe dede thakur {str}"
+    print(printhar(str))
+
+if __name__ == '__main__':
+    def add(num1, num2):
+     return num1 + num2 +5
+    o = add(4, 6)
+print(o)
+
+# o = "num1"
+# o = int(input("Enter the number: "))
+# p = 'num2'
+# p = int(input("Enter the number: "))
+# print(o+p)
+#
