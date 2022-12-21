8587c8cb78f0d7368e31cd0ae0058e15536d699c
naitikjn <trendytech29@gmail.com>
2022-12-21 20:15:48 +0530

    hello

--- practice.py
+++ practice.py
@@ -1298,3 +1298,30 @@ if __name__ == "__main__":
         else:
             print("No query matched")
 >>>>>>> 66b585c (hello)
+
+# var1 = "Hello "
+# var2 = "Trendy tech"
+# var3 = 56
+# var4 = 14
+#
+# print(str(var1) + str(var2))
+# print(100* "hello world \n")
+# print("enter your number")
+# abcd = input()
+#
+# print("you entered", int(abcd) + 10)
+#
+# print("Enter the number")
+# n1 = input()
+# print("Enter the number")
+# n2 = input()
+# print("Sum of these numbers: ", int(n1) + int(n2))
+#
+# # Data types
+# """
+# 1. int()
+# 2. float()
+# 3. str()
+
+# variables & data types
+a =1
