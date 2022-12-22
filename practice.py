9343780ec01e07a618374cd8bc395ee8523824ea
naitikjn <trendytech29@gmail.com>
2022-12-22 20:45:22 +0530

    hello

--- practice.py
+++ practice.py
@@ -1347,3 +1347,25 @@ print(o)
 # p = int(input("Enter the number: "))
 # print(o+p)
 #
+
+
+# a = 5             # Global
+# def function1(x):
+#     a
+#     m = 5         # Local
+#     print(a,m)
+#     print(x,"this is me")
+#
+# function1("I have printed")
+# print(a)
+a = 10
+def function1(x):
+
+    m = 5         # Local
+    global a
+    a = a + 10
+    print(a,m)
+    print(x,"this is me")
+
+function1("I have printed")
+print(a)
