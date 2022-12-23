e6742470c69ed0c1ca16180ce826b12ea5e5378e
naitikjn <trendytech29@gmail.com>
2022-12-23 19:16:06 +0530

    hello

--- Data types.py
+++ Data types.py
@@ -23,12 +23,18 @@
 # 3. str()
 
 # variables & data types
-a =1
-print(a)
-b = 23
-print(a+b)
+fruit = "Mango"
+mangoLen = len(fruit)
+print(mangoLen)
+# print(fruit[0:4]) # including 0 but not 4
+# print(fruit[1:4]) # including 1 but not 4
+# print(fruit[:5])
+# print(fruit[0:-3])
+# print(fruit[:len(fruit)-3])
+print(fruit[-1:len(fruit) - 3])
+print(fruit[-3:-1])
 
-
-
-
-
+# Quick Quiz:
+# nm = "Harry"
+# print(nm[-4:-2])
+# @codewithharry
