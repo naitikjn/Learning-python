5548704c8a13b66c9c75cf91b99f0562c4cdd32d
naitikjn <trendytech29@gmail.com>
2022-12-23 19:05:28 +0530

    hello

--- practice.py
+++ practice.py
@@ -1369,3 +1369,26 @@ def function1(x):
 
 function1("I have printed")
 print(a)
+
+
+# a = 5             # Global
+# def function1(x):
+#     a
+#     m = 5         # Local
+#     print(a,m)
+#     print(x,"this is me")
+#
+# function1("I have printed")
+# print(a)
+a = 10
+def function1(x):
+
+    m = 5         # Local
+    global a
+    a = a + 10
+    print(a,m)
+    print(x,"this is me")
+
+function1("I have printed")
+print(a)
+
