dddd071263b447545e16374efffadbf31e585b54
naitikjn <trendytech29@gmail.com>
2022-12-27 19:29:54 +0530

    hello

--- first_class.py
+++ first_class.py
@@ -1,80 +1,93 @@
-class Student:
-    pass
+# class Student:
+#     pass
+#
+#
+# harry = Student()
+# larry = Student()
+#
+# harry.name = "Harry"
+# harry.std = 12
+# harry.section = 1
+# larry.std = 9
+# larry.subjects = ["hindi", "physics"]
+# print(harry.section, larry.subjects)
+#
+#
+# class Employee:
+#     no_of_leaves = 8
+#     pass
+#
+# harry = Employee()
+# rohan = Employee()
+#
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+#
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+#
+# print(Employee.no_of_leaves)
+# print(Employee.__dict__)
+# Employee.no_of_leaves = 9
+# print(Employee.__dict__)
+# print(Employee.no_of_leaves)
+#
+#
+#
+#
+#
+# class Student:
+#     pass
+#
+#
+# harry = Student()
+# larry = Student()
+#
+# harry.name = "Harry"
+# harry.std = 12
+# harry.section = 1
+# larry.std = 9
+# larry.subjects = ["hindi", "physics"]
+# print(harry.section, larry.subjects)
+#
+#
+# class Employee:
+#     no_of_leaves = 8
+#     pass
+#
+# harry = Employee()
+# rohan = Employee()
+#
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+#
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+#
+# print(Employee.no_of_leaves)
+# print(Employee.__dict__)
+# Employee.no_of_leaves = 9
+# print(Employee.__dict__)
+# print(Employee.no_of_leaves)
 
 
-harry = Student()
-larry = Student()
-
-harry.name = "Harry"
-harry.std = 12
-harry.section = 1
-larry.std = 9
-larry.subjects = ["hindi", "physics"]
-print(harry.section, larry.subjects)
 
 
-class Employee:
-    no_of_leaves = 8
-    pass
+for pygame import mixer
+def musiconloop(file,stopper):
+    mixer.init()
+    mixer.music.load(file)
+    mixer.music.play()
+    while True:
+        a = input()
+        if a == stopper:
+            mixer.music.stop()
+            break
 
-harry = Employee()
-rohan = Employee()
-
-harry.name = "Harry"
-harry.salary = 455
-harry.role = "Instructor"
-
-rohan.name = "Rohan"
-rohan.salary = 4554
-rohan.role = "Student"
-
-print(Employee.no_of_leaves)
-print(Employee.__dict__)
-Employee.no_of_leaves = 9
-print(Employee.__dict__)
-print(Employee.no_of_leaves)
-
-
-
-
-
-class Student:
-    pass
-
-
-harry = Student()
-larry = Student()
-
-harry.name = "Harry"
-harry.std = 12
-harry.section = 1
-larry.std = 9
-larry.subjects = ["hindi", "physics"]
-print(harry.section, larry.subjects)
-
-
-class Employee:
-    no_of_leaves = 8
-    pass
-
-harry = Employee()
-rohan = Employee()
-
-harry.name = "Harry"
-harry.salary = 455
-harry.role = "Instructor"
-
-rohan.name = "Rohan"
-rohan.salary = 4554
-rohan.role = "Student"
-
-print(Employee.no_of_leaves)
-print(Employee.__dict__)
-Employee.no_of_leaves = 9
-print(Employee.__dict__)
-print(Employee.no_of_leaves)
-
-
-
-
+if __name__ == '__main__':
+    musiconloop("water.mp3","stop")
 
