978dc8b60ef57f6b364c926b4dfa9d4ce1adf1e2
naitikjn <trendytech29@gmail.com>
2022-12-28 19:23:52 +0530

    hello

--- if_Else_Elif.py
+++ if_Else_Elif.py
@@ -1,1 +1,13 @@
-Naitik bhai bahut achhe hai
+print("WELCOME TO TRENDY TECH")
+print("Checking for Driving Licence")
+print("2022 - 23")
+
+X = float(input("Enter your age :"))
+print("Your age is : ",X)
+if(X>=18):
+    print("You can drive, quickly make your licence")
+elif(X<=18):
+    print("Wait for some time")
+else:
+    X<18
+    print("Thank you so much. Please come again")
