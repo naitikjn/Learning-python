da1ecc0fa582f7f7cbb6c657376524f1bf364a7b
naitikjn <trendytech29@gmail.com>
2022-12-31 20:54:44 +0530

    hello

--- practice.py
+++ practice.py
@@ -1404,7 +1404,22 @@ print(a)
 # print(a)
 a = 10
 def function1(x):
+    x = int(input("Enter the value of x: "))
+    # x is the variable to match
+    match x:
+        # if x is 0
+        case 0:
+            print("x is zero")
+        # case with if-condition
+        case 4:
+            print("case is 4")
 
+        case _ if x != 90:
+            print(x, "is not 90")
+        case _ if x != 80:
+            print(x, "is not 80")
+        case _:
+            print(x)
     m = 5         # Local
     global a
     a = a + 10
