3b2f5cb15bb63baf87fad114c3ccd7ac49ef0694
naitikjn <trendytech29@gmail.com>
2023-01-03 16:32:49 +0530

    hello

--- List_functions.py
+++ List_functions.py
@@ -1,35 +1,39 @@
-#lists in python
-#groceries = ["Nescafe","Horlicks","No.1","fena","Wheel","Vim bar","Lollypop"]
-#print(groceries[0:5])
-Numbers = [2,5,4,9,7]
-#Numbers.sort()
-#Numbers.reverse()
-print(max(Numbers))
-print(min(Numbers))
-Numbers.append(5)
-print(Numbers)
-Numbers = []
-Numbers.append(25)
-Numbers.append(15)
-Numbers.append(26)
-#Numbers.sort()
-#Numbers.reverse()
-Numbers.insert(2 ,5)
-Numbers.remove(5)
-#Numbers.pop()
-Numbers[2] = 2
-print(Numbers)
-# Mutable = can change
-# Immutable = can not change
-# Tuple
-tp = (1,2,3)
-print(tp)
-a = 1
-b = 8
-temp = a
-a = b
-temp = b
-print(a,b)
-groceries = ["Nescafe","Horlicks","No.1","fena","Wheel"]
-print(groceries)
-print("exit code")
+# #lists in python
+# #groceries = ["Nescafe","Horlicks","No.1","fena","Wheel","Vim bar","Lollypop"]
+# #print(groceries[0:5])
+# Numbers = [2,5,4,9,7]
+# #Numbers.sort()
+# #Numbers.reverse()
+# print(max(Numbers))
+# print(min(Numbers))
+# Numbers.append(5)
+# print(Numbers)
+# Numbers = []
+# Numbers.append(25)
+# Numbers.append(15)
+# Numbers.append(26)
+# #Numbers.sort()
+# #Numbers.reverse()
+# Numbers.insert(2 ,5)
+# Numbers.remove(5)
+# #Numbers.pop()
+# Numbers[2] = 2
+# print(Numbers)
+# # Mutable = can change
+# # Immutable = can not change
+# # Tuple
+# tp = (1,2,3)
+# print(tp)
+# a = 1
+# b = 8
+# temp = a
+# a = b
+# temp = b
+# print(a,b)
+# groceries = ["Nescafe","Horlicks","No.1","fena","Wheel"]
+# print(groceries)
+# print("exit code")
+l = ['abc',23,'mouse',[0,1,2,3]]
+print(l)
+l.append(5)
+print(l)
