10631a5f206126fa42d2f416b8d19ef1bac54b14
naitikjn <trendytech29@gmail.com>
2023-01-06 16:45:03 +0530

    hello

--- Functions.py
+++ Functions.py
@@ -1,25 +1,36 @@
-#Functions or Docstring
+# #Functions or Docstring
+#
+# a = 9
+# b = 8
+# c = sum((a,b))
+# print(c)
+#
+# def function1(a,b):
+#     print("Hello you are in function1",a+b)
+#
+#
+# function1()
+# function1()
+# function1()
+# function1()
+#
+# def function2(a,b):
+#     average =(a+b)/2
+#     print(average)
+# function2(56,67)
+#
+#
+# num1 = input("1")
+# print(num1)
 
-a = 9
-b = 8
-c = sum((a,b))
-print(c)
+a = 10
+b = 10
+gmean = (a*b)/(a+b)
+print(gmean)
 
-def function1(a,b):
-    print("Hello you are in function1",a+b)
-
-
-function1()
-function1()
-function1()
-function1()
-
-def function2(a,b):
-    average =(a+b)/2
-    print(average)
-function2(56,67)
-
-
-num1 = input("1")
-print(num1)
+def isgreator(a,b):
+    if (a>b):
+        print("A is greator")
+    else:
+        print("B is greator")
 
