4e30bd329ee66fe8a48b10fa41effd501844ffe6
naitikjn <trendytech29@gmail.com>
2023-01-09 16:34:00 +0530

    hello

--- Tuples.py
+++ Tuples.py
@@ -7,7 +7,14 @@ print(tup[1])
 print(tup[2])
 print(tup[3])
 print(tup[4])
-
-
+i = int(input("Enter the number: "))
+if i in tup:
+    print("YES it is in tup")
+else:
+    print("NO")
+tup2 = tup[1:4]
+print(tup2)
+tup.extend(tup2)
+print(tup)
 
 
