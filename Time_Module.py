fafa671ef7499cfe85b5bd2d3c0dc785204c127b
naitikjn <trendytech29@gmail.com>
2023-01-10 16:09:21 +0530

    hello

--- Time_Module.py
+++ Time_Module.py
@@ -27,5 +27,15 @@ t = time.strftime('%H:%M:%S')
 #   print("Good Afternoon Sir!")
 # elif(hour>=17 and hour<=24):
 #   print("Good Night sir!")
+# hour = int(time.strftime('%H'))
+# hour = int(input("Enter hour: "))
+# print(hour)
+#
+# if(hour>=0 and hour<12):
+#   print("Good Morning Sir!")
+# elif(hour>=12 and hour<17):
+#   print("Good Afternoon Sir!")
+# elif(hour>=17 and hour<=24):
+#   print("Good Night sir!")
 
 print(t, "Today time")
