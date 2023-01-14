dbb3bda75c272dcf30de41865fe4462a938c6d74
naitikjn <trendytech29@gmail.com>
2023-01-14 16:30:17 +0530

    hello

--- first_class.py
+++ first_class.py
@@ -172,3 +172,31 @@ if __name__ == '__main__':
 
 for pygame import mixer
 def musiconloop(file,stopper
+#     no_of_leaves = 8
+#     pass
+#
+# harry = Employee()
+# rohan = Employee()
+#
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+#
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+#
+# print(Employee.no_of_leaves)
+# print(Employee.__dict__)
+# Employee.no_of_leaves = 9
+# print(Employee.__dict__)
+# print(Employee.no_of_leaves)
+
+    for
+
+
+pygame
+import mixer
+
+
+def musiconloop(file, stopper
