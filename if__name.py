3aa6a641abd5c3c3ba895dfa90405fcc9fd8ff66
naitikjn <trendytech29@gmail.com>
2023-01-14 16:18:29 +0530

    hello

--- if__name.py
+++ if__name.py
@@ -160,3 +160,59 @@ if __name__ == '__main__':
 
 
 
+            while True:
+                if time() - init_water > watersecs:
+                    print("Water Drinking time. Enter 'drank' to stop the alarm.")
+                    musiconloop('water.mp3', 'drank')
+                    init_water = time()
+                    log_now("Drank Water at")
+
+                if time() - init_eyes > eyessecs:
+                    print("Eye exercise time. Enter 'doneeyes' to stop the alarm.")
+                    musiconloop('eyes.mp3', 'doneeyes')
+                    init_eyes = time()
+                    log_now("Eyes Relaxed at")
+
+                if time() - init_exercise > exsecs:
+                    print("Physical Activity Time. Enter 'donephy' to stop the alarm.")
+                    musiconloop('physical.mp3', 'donephy')
+                    init_exercise = time()
+                    log_now("Physical Activity done at")
+
+            break
+
+def log_now(msg):
+    with open("mylogs.txt", "a") as f:
+        f.write(f"{msg} {datetime.now()}\n")
+
+if __name__ == '__main__':
+    # musiconloop("water.mp3", "stop")
+    init_water = time()
+    init_eyes = time()
+    init_exercise = time()
+    watersecs = 40*60
+    exsecs = 30*60
+    eyessecs = 45*60
+
+    while True:
+        if time() - init_water > watersecs:
+            print("Water Drinking time. Enter 'drank' to stop the alarm.")
+            musiconloop('water.mp3', 'drank')
+            init_water = time()
+            log_now("Drank Water at")
+
+        if time() - init_eyes >eyessecs:
+            print("Eye exercise time. Enter 'doneeyes' to stop the alarm.")
+            musiconloop('eyes.mp3', 'doneeyes')
+            init_eyes = time()
+            log_now("Eyes Relaxed at")
+
+        if time() - init_exercise > exsecs:
+            print("Physical Activity Time. Enter 'donephy' to stop the alarm.")
+            musiconloop('physical.mp3', 'donephy')
+            init_exercise = time()
+            log_now("Physical Activity done at")
+
+
+
+
