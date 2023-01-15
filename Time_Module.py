17ce91e8e39413ee833438b98c21acca797073e5
naitikjn <trendytech29@gmail.com>
2023-01-15 14:28:51 +0530

    hello

--- Time_Module.py
+++ Time_Module.py
@@ -1,27 +1,165 @@
-
-# intial = time.time()
-# k = 0
-# while (k<45):
-#      print("This is Trendy tech")
-#      time.sleep(10)
-#      k+=1
 #
-# print("While loop ran in", time.time() - intial,"seconds")
+# # intial = time.time()
+# # k = 0
+# # while (k<45):
+# #      print("This is Trendy tech")
+# #      time.sleep(10)
+# #      k+=1
+# #
+# # print("While loop ran in", time.time() - intial,"seconds")
+# #
+# # intial2 = time.time()
+# # for x in range (45):
+# #    print("This is Trendy tech")
+# #    print("for loop ran in",time.time() - intial2,'seconds')
 #
-# intial2 = time.time()
-# for x in range (45):
-#    print("This is Trendy tech")
-#    print("for loop ran in",time.time() - intial2,'seconds')
-
-# localtime = time.asctime(time.localtime(time.time()))
-# print(localtime)
-import time
-t = time.strftime('%H:%M:%S')
-# hour = int(time.strftime('%H'))
-# hour = int(input("Enter hour: "))
-# print(hour)
+# # localtime = time.asctime(time.localtime(time.time()))
+# # print(localtime)
+# import time
+# t = time.strftime('%H:%M:%S')
+# # hour = int(time.strftime('%H'))
+# # hour = int(input("Enter hour: "))
+# # print(hour)
+# #
+# # if(hour>=0 and hour<12):
+# #   print("Good Morning Sir!")
+# # elif(hour>=12 and hour<17):
+# #   print("Good Afternoon Sir!")
+# # elif(hour>=17 and hour<=24):
+# #   print("Good Night sir!")
+# # hour = int(time.strftime('%H'))
+# # hour = int(input("Enter hour: "))
+# # print(hour)
+# #
+# # if(hour>=0 and hour<12):
+# #   print("Good Morning Sir!")
+# # elif(hour>=12 and hour<17):
+# #   print("Good Afternoon Sir!")
+# # elif(hour>=17 and hour<=24):
+# #   print("Good Night sir!")
 #
-# if(hour>=0 and hour<12):
+# print(t, "Today time")
+#
+# # intial = time.time()
+# # k = 0
+# # while (k<45):
+# #      print("This is Trendy tech")
+# #      time.sleep(10)
+# #      k+=1
+# #
+# # print("While loop ran in", time.time() - intial,"seconds")
+# #
+# # intial2 = time.time()
+# # for x in range (45):
+# #    print("This is Trendy tech")
+# #    print("for loop ran in",time.time() - intial2,'seconds')
+#
+# # localtime = time.asctime(time.localtime(time.time()))
+# # print(localtime)
+# import time
+# t = time.strftime('%H:%M:%S')
+# # hour = int(time.strftime('%H'))
+# # hour = int(input("Enter hour: "))
+# # print(hour)
+# #
+# # if(hour>=0 and hour<12):
+# #   print("Good Morning Sir!")
+# # elif(hour>=12 and hour<17):
+# #   print("Good Afternoon Sir!")
+# # elif(hour>=17 and hour<=24):
+# #   print("Good Night sir!")
+# # hour = int(time.strftime('%H'))
+# # hour = int(input("Enter hour: "))
+# # print(hour)
+# #
+# # if(hour>=0 and hour<12):
+# #   print("Good Morning Sir!")
+# # elif(hour>=12 and hour<17):
+# #   print("Good Afternoon Sir!")
+# # elif(hour>=17 and hour<=24):
+# #   print("Good Night sir!")
+# # intial = time.time()
+# # k = 0
+# # while (k<45):
+# #      print("This is Trendy tech")
+# #      time.sleep(10)
+# #      k+=1
+# #
+# # print("While loop ran in", time.time() - intial,"seconds")
+# #
+# # intial2 = time.time()
+# # for x in range (45):
+# #    print("This is Trendy tech")
+# #    print("for loop ran in",time.time() - intial2,'seconds')
+#
+# # localtime = time.asctime(time.localtime(time.time()))
+# # print(localtime)
+# import time
+# t = time.strftime('%H:%M:%S')
+# # hour = int(time.strftime('%H'))
+# # hour = int(input("Enter hour: "))
+# # print(hour)
+# #
+# # if(hour>=0 and hour<12):
+# #   print("Good Morning Sir!")
+# # elif(hour>=12 and hour<17):
+# #   print("Good Afternoon Sir!")
+# # elif(hour>=17 and hour<=24):
+# #   print("Good Night sir!")
+# # hour = int(time.strftime('%H'))
+# # hour = int(input("Enter hour: "))
+# # print(hour)
+# #
+# # if(hour>=0 and hour<12):
+# #   print("Good Morning Sir!")
+# # elif(hour>=12 and hour<17):
+# #   print("Good Afternoon Sir!")
+# # elif(hour>=17 and hour<=24):
+# #   print("Good Night sir!")
+#
+# print(t, "Today time")
+#
+# # intial = time.time()
+# # k = 0
+# # while (k<45):
+# #      print("This is Trendy tech")
+# #      time.sleep(10)
+# #      k+=1
+# #
+# # print("While loop ran in", time.time() - intial,"seconds")
+# #
+# # intial2 = time.time()
+# # for x in range (45):
+# #    print("This is Trendy tech")
+# #    print("for loop ran in",time.time() - intial2,'seconds')
+#
+# # localtime = time.asctime(time.localtime(time.time()))
+# # print(localtime)
+# import time
+# t = time.strftime('%H:%M:%S')
+# # hour = int(time.strftime('%H'))
+# # hour = int(input("Enter hour: "))
+# # print(hour)
+# #
+# # if(hour>=0 and hour<12):
+# #   print("Good Morning Sir!")
+# # elif(hour>=12 and hour<17):
+# #   print("Good Afternoon Sir!")
+# # elif(hour>=17 and hour<=24):
+# #   print("Good Night sir!")
+# # hour = int(time.strftime('%H'))
+# # hour = int(input("Enter hour: "))
+# # print(hour)
+# #
+# # if(hour>=0 and hour<12):
+# #   print("Good Morning Sir!")
+# # elif(hour>=12 and hour<17):
+# #   print("Good Afternoon Sir!")
+# # elif(hour>=17 and hour<=24):
+# #   print("Good Night sir!")
+# # print(hour)
+# #
+# # if(hour>=0 and hour<12):
 #   print("Good Morning Sir!")
 # elif(hour>=12 and hour<17):
 #   print("Good Afternoon Sir!")
@@ -38,143 +176,114 @@ t = time.strftime('%H:%M:%S')
 # elif(hour>=17 and hour<=24):
 #   print("Good Night sir!")
 
-print(t, "Today time")
 
-# intial = time.time()
-# k = 0
-# while (k<45):
-#      print("This is Trendy tech")
-#      time.sleep(10)
-#      k+=1
-#
-# print("While loop ran in", time.time() - intial,"seconds")
-#
-# intial2 = time.time()
-# for x in range (45):
-#    print("This is Trendy tech")
-#    print("for loop ran in",time.time() - intial2,'seconds')
+import os
+import pandas as pd
+from pydub import AudioSegment
+from gtts import gTTS
 
-# localtime = time.asctime(time.localtime(time.time()))
-# print(localtime)
-import time
-t = time.strftime('%H:%M:%S')
-# hour = int(time.strftime('%H'))
-# hour = int(input("Enter hour: "))
-# print(hour)
-#
-# if(hour>=0 and hour<12):
-#   print("Good Morning Sir!")
-# elif(hour>=12 and hour<17):
-#   print("Good Afternoon Sir!")
-# elif(hour>=17 and hour<=24):
-#   print("Good Night sir!")
-# hour = int(time.strftime('%H'))
-# hour = int(input("Enter hour: "))
-# print(hour)
-#
-# if(hour>=0 and hour<12):
-#   print("Good Morning Sir!")
-# elif(hour>=12 and hour<17):
-#   print("Good Afternoon Sir!")
-# elif(hour>=17 and hour<=24):
-#   print("Good Night sir!")
-# intial = time.time()
-# k = 0
-# while (k<45):
-#      print("This is Trendy tech")
-#      time.sleep(10)
-#      k+=1
-#
-# print("While loop ran in", time.time() - intial,"seconds")
-#
-# intial2 = time.time()
-# for x in range (45):
-#    print("This is Trendy tech")
-#    print("for loop ran in",time.time() - intial2,'seconds')
 
-# localtime = time.asctime(time.localtime(time.time()))
-# print(localtime)
-import time
-t = time.strftime('%H:%M:%S')
-# hour = int(time.strftime('%H'))
-# hour = int(input("Enter hour: "))
-# print(hour)
-#
-# if(hour>=0 and hour<12):
-#   print("Good Morning Sir!")
-# elif(hour>=12 and hour<17):
-#   print("Good Afternoon Sir!")
-# elif(hour>=17 and hour<=24):
-#   print("Good Night sir!")
-# hour = int(time.strftime('%H'))
-# hour = int(input("Enter hour: "))
-# print(hour)
-#
-# if(hour>=0 and hour<12):
-#   print("Good Morning Sir!")
-# elif(hour>=12 and hour<17):
-#   print("Good Afternoon Sir!")
-# elif(hour>=17 and hour<=24):
-#   print("Good Night sir!")
+# pip install pyaudio
+# pip install pydub
+# pip install pandas
+# pip install gTTS
 
-print(t, "Today time")
 
-# intial = time.time()
-# k = 0
-# while (k<45):
-#      print("This is Trendy tech")
-#      time.sleep(10)
-#      k+=1
-#
-# print("While loop ran in", time.time() - intial,"seconds")
-#
-# intial2 = time.time()
-# for x in range (45):
-#    print("This is Trendy tech")
-#    print("for loop ran in",time.time() - intial2,'seconds')
+def textToSpeech(text, filename):
+    mytext = str(text)
+    language = 'hi'
+    myobj = gTTS(text=mytext, lang=language, slow=False)
+    myobj.save(filename)
 
-# localtime = time.asctime(time.localtime(time.time()))
-# print(localtime)
-import time
-t = time.strftime('%H:%M:%S')
-# hour = int(time.strftime('%H'))
-# hour = int(input("Enter hour: "))
-# print(hour)
-#
-# if(hour>=0 and hour<12):
-#   print("Good Morning Sir!")
-# elif(hour>=12 and hour<17):
-#   print("Good Afternoon Sir!")
-# elif(hour>=17 and hour<=24):
-#   print("Good Night sir!")
-# hour = int(time.strftime('%H'))
-# hour = int(input("Enter hour: "))
-# print(hour)
-#
-# if(hour>=0 and hour<12):
-#   print("Good Morning Sir!")
-# elif(hour>=12 and hour<17):
-#   print("Good Afternoon Sir!")
-# elif(hour>=17 and hour<=24):
-#   print("Good Night sir!")
-# print(hour)
-#
-# if(hour>=0 and hour<12):
-#   print("Good Morning Sir!")
-# elif(hour>=12 and hour<17):
-#   print("Good Afternoon Sir!")
-# elif(hour>=17 and hour<=24):
-#   print("Good Night sir!")
-# hour = int(time.strftime('%H'))
-# hour = int(input("Enter hour: "))
-# print(hour)
-#
-# if(hour>=0 and hour<12):
-#   print("Good Morning Sir!")
-# elif(hour>=12 and hour<17):
-#   print("Good Afternoon Sir!")
-# elif(hour>=17 and hour<=24):
-#   print("Good Night sir!")
+
+# This function returns pydubs audio segment
+def mergeAudios(audios):
+    combined = AudioSegment.empty()
+    for audio in audios:
+        combined += AudioSegment.from_mp3(audio)
+    return combined
+
+
+def generateSkeleton():
+    audio = AudioSegment.from_mp3('railway.mp3')
+
+    # 1 - Generate kripya dheyan dijiye
+    start = 88000
+    finish = 90200
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("1_hindi.mp3", format="mp3")
+
+    # 2 is from-city
+
+    # 3 - Generate se chalkar
+    start = 91000
+    finish = 92200
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("3_hindi.mp3", format="mp3")
+
+    # 4 is via-city
+
+    # 5 - Generate ke raaste
+    start = 94000
+    finish = 95000
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("5_hindi.mp3", format="mp3")
+
+    # 6 is to-city
+
+    # 7 - Generate ko jaane wali gaadi sakhya
+    start = 96000
+    finish = 98900
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("7_hindi.mp3", format="mp3")
+
+    # 8 is train no and name
+
+    # 9 - Generate kuch hi samay mei platform sankhya
+    start = 105500
+    finish = 108200
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("9_hindi.mp3", format="mp3")
+
+    # 10 is platform number
+
+    # 11 - Generate par aa rahi hai
+    start = 109000
+    finish = 112250
+    audioProcessed = audio[start:finish]
+    audioProcessed.export("11_hindi.mp3", format="mp3")
+
+
+def generateAnnouncement(filename):
+    df = pd.read_excel(filename)
+    print(df)
+    for index, item in df.iterrows():
+        # 2 - Generate from-city
+        textToSpeech(item['from'], '2_hindi.mp3')
+
+        # 4 - Generate via-city
+        textToSpeech(item['via'], '4_hindi.mp3')
+
+        # 6 - Generate to-city
+        textToSpeech(item['to'], '6_hindi.mp3')
+
+        # 8 - Generate train no and name
+        textToSpeech(item['train_no'] + " " + item['train_name'], '8_hindi.mp3')
+
+        # 10 - Generate platform number
+        textToSpeech(item['platform'], '10_hindi.mp3')
+
+        audios = [f"{i}_hindi.mp3" for i in range(1, 12)]
+
+        announcement = mergeAudios(audios)
+        announcement.export(f"announcement_{item['train_no']}_{index + 1}.mp3", format="mp3")
+
+
+if __name__ == "__main__":
+    print("Generating Skeleton...")
+    generateSkeleton()
+    print("Now Generating Announcement...")
+    generateAnnouncement("announce_hindi.xlsx")
 
 
 
