7f8e4ef4f3f9d4944efdcbcb6f9c52c9cc18d26b
naitikjn <trendytech29@gmail.com>
2023-01-17 16:25:38 +0530

    hello

--- practice.py
+++ practice.py
@@ -1461,3 +1461,205 @@
 #
 # function1("I have printed")
 # print(a)
+
+#     pass
+#
+#
+# harry = Student()
+# larry = Student()
+#
+# harry.name = "Harry"
+# harry.std = 12
+# harry.section = 1
+# larry.std = 9
+# larry.subjects = ["hindi", "physics"]
+# print(harry.section, larry.subjects)
+#
+#
+# class Employee:
+#     no_of_leaves = 8
+#     pass
+#
+# harry = Employee()
+# rohan = Employee()
+#
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+#
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+#
+# print(Employee.no_of_leaves)
+# print(Employee.__dict__)
+# Employee.no_of_leaves = 9
+# print(Employee.__dict__)
+# print(Employee.no_of_leaves)
+#
+#
+#
+#
+#
+# class Student:
+#     pass
+#
+#
+# harry = Student()
+# larry = Student()
+#
+# harry.name = "Harry"
+# harry.std = 12
+# harry.section = 1
+# larry.std = 9
+# larry.subjects = ["hindi", "physics"]
+# print(harry.section, larry.subjects)
+#
+#
+# class Employee:
+#     no_of_leaves = 8
+#     pass
+#
+# harry = Employee()
+# rohan = Employee()
+#
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+#
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+#
+# print(Employee.no_of_leaves)
+# print(Employee.__dict__)
+# Employee.no_of_leaves = 9
+# print(Employee.__dict__)
+# print(Employee.no_of_leaves)
+
+
+
+
+for pygame import mixer
+def musiconloop(file,stopper):
+    mixer.init()
+    mixer.music.load(file)
+    mixer.music.play()
+    while True:
+        a = input()
+        if a == stopper:
+            mixer.music.stop()
+            break
+
+if __name__ == '__main__':
+    musiconloop("water.mp3","stop")
+
+# class Student:
+#     pass
+#
+#
+# harry = Student()
+# larry = Student()
+#
+# harry.name = "Harry"
+# harry.std = 12
+# harry.section = 1
+# larry.std = 9
+# larry.subjects = ["hindi", "physics"]
+# print(harry.section, larry.subjects)
+#
+#
+# class Employee:
+#     no_of_leaves = 8
+#     pass
+#
+# harry = Employee()
+# rohan = Employee()
+#
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+#
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+#
+# print(Employee.no_of_leaves)
+# print(Employee.__dict__)
+# Employee.no_of_leaves = 9
+# print(Employee.__dict__)
+# print(Employee.no_of_leaves)
+#
+#
+#
+#
+#
+# class Student:
+#     pass
+#
+#
+# harry = Student()
+# larry = Student()
+#
+# harry.name = "Harry"
+# harry.std = 12
+# harry.section = 1
+# larry.std = 9
+# larry.subjects = ["hindi", "physics"]
+# print(harry.section, larry.subjects)
+#
+#
+# class Employee:
+#     no_of_leaves = 8
+#     pass
+#
+# harry = Employee()
+# rohan = Employee()
+#
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+#
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+#
+# print(Employee.no_of_leaves)
+# print(Employee.__dict__)
+# Employee.no_of_leaves = 9
+# print(Employee.__dict__)
+# print(Employee.no_of_leaves)
+
+
+
+
+for pygame import mixer
+def musiconloop(file,stopper
+#     no_of_leaves = 8
+#     pass
+#
+# harry = Employee()
+# rohan = Employee()
+#
+# harry.name = "Harry"
+# harry.salary = 455
+# harry.role = "Instructor"
+#
+# rohan.name = "Rohan"
+# rohan.salary = 4554
+# rohan.role = "Student"
+#
+# print(Employee.no_of_leaves)
+# print(Employee.__dict__)
+# Employee.no_of_leaves = 9
+# print(Employee.__dict__)
+# print(Employee.no_of_leaves)
+
+    for
+
+
+pygame
+import mixer
+
+
+def musiconloop(file, stopper
