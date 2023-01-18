af38b434824b8102b73f6df8e35973d21e69d9bd
naitikjn <trendytech29@gmail.com>
2023-01-18 16:26:51 +0530

    hello

--- F_string.py
+++ F_string.py
@@ -21,17 +21,17 @@
 # # b = a.format(me, a1)
 # # print(b)
 a = f"this is {me} {a1} {math.cos(65)}"
-# time
-print(a)
-
-
-# me = "Harry"
-# a1 =3
-# # a = "this is %s %s"%(me, a1)
-# # a = "This is {1} {0}"
-# # b = a.format(me, a1)
-# # print(b)
-a = f"this is {me} {a1} {math.cos(65)}"
-# time
-print(a)
-
+# # time
+# print(a)
+#
+#
+# # me = "Harry"
+# # a1 =3
+# # # a = "this is %s %s"%(me, a1)
+# # # a = "This is {1} {0}"
+# # # b = a.format(me, a1)
+# # # print(b)
+# a = f"this is {me} {a1} {math.cos(65)}"
+# # time
+# print(a)
+#
