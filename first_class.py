3300873d1012fbdc85f6ed5086ecc6aeadb1be1e
naitikjn <trendytech29@gmail.com>
2023-01-24 20:26:08 +0530

    hello

--- first_class.py
+++ first_class.py
@@ -49,3 +49,38 @@
 # # Employee.no_of_leaves = 9
 # # print(Employee.__dict__)
 # # print(Employee.no_of_leaves)
+# string = input("Enter a string :")
+# count = 3
+# while True:
+#     if string[0] == 'a':
+#         string=string [2:]
+#     elif string [-1] == 'b':
+#         string = string [:2]
+#     else:
+#         count+=1
+#         break
+# print(string)
+# print(count)
+
+# s = "CBSE digital india"
+# for i in range(len(s)-1,0,-1):
+#     if s[i].isupper():
+#         print(s[i].lower(),end='')
+#     elif i%2==0:
+#         if s[i].islower():
+#             print(s[i].upper(),end='')
+#     else:
+#         print("@" ,end='')
+a = "EXAM2021"
+b = " "
+i = 0
+while i<len(a):
+    if a[i]>="A" and a[i]<= 'M':
+        b =b + a[i+1]
+    elif a[i] >= "0" and a[i] <= '9':
+        b =b + a[i-1]
+
+    else:
+        b = b + '*'
+    i = i + 1
+print(b)
