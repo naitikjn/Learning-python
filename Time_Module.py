6072ceb4031f65bd4a4da20477db1b40cdfc1def
naitikjn <trendytech29@gmail.com>
2023-01-27 20:04:39 +0530

    hello

--- Time_Module.py
+++ Time_Module.py
@@ -200,10 +200,3 @@
 #
 #      announcement.export(f"announcement_{item['train_no']}_{index + 1}.mp3", format="mp3")
 #
-#
-# if __name__ == "__main__":
-#     print("Generating Skeleton...")
-#     generateSkeleton()
-#     print("Now Generating Announcement...")
-#     generateAnnouncement("announce_hindi.xlsx")
-
