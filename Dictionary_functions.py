bd97e721ee50802d312a20f3ff47dd3699c66fcc
naitikjn <trendytech29@gmail.com>
2023-02-06 20:55:50 +0530

    hello

--- Dictionary_functions.py
+++ Dictionary_functions.py
@@ -1,16 +1,16 @@
-# # Dictionary in python
-# d1 = { }
-# print(d1)
-# d2 = {"Ram": "pasta","Shyam": "Maggi","Kohli": "Bathure","Rohit": {"B": "Poha", "L": "Dal roti","D": "Pav bhaji"}}
-# d2["Ankit"] = "Junk food"
-# d2["Sourav"] = "Dal tikad"
-# #print(d2["Rohit"])
-# #print(d2["Rohit"]["B"])
-# #del d2["Ram"]
-# #d3 = d2.copy()
-# #print(d2.get("Kohli"))
-# d2.update({"leena": "Coffee"})
-# print(d2.keys())
-# print(d2.items())
-# empdict = {'Eno' : [1,2,3,4] , 'Ename' : ['Raj','Seema','John','Smith'],'Sal': [1000,2000,3000,4000]}
-# print(empdict['Ename'][0],empdict['Sal'][0])
+# Dictionary in python
+d1 = { }
+print(d1)
+d2 = {"Ram": "pasta","Shyam": "Maggi","Kohli": "Bathure","Rohit": {"B": "Poha", "L": "Dal roti","D": "Pav bhaji"}}
+d2["Ankit"] = "Junk food"
+d2["Sourav"] = "Dal tikad"
+#print(d2["Rohit"])
+#print(d2["Rohit"]["B"])
+#del d2["Ram"]
+#d3 = d2.copy()
+#print(d2.get("Kohli"))
+d2.update({"leena": "Coffee"})
+print(d2.keys())
+print(d2.items())
+empdict = {'Eno' : [1,2,3,4] , 'Ename' : ['Raj','Seema','John','Smith'],'Sal': [1000,2000,3000,4000]}
+print(empdict['Ename'][0],empdict['Sal'][0])
