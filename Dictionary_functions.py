67d2ecf8f877b846155067bc3ae5be34f382e004
naitikjn <trendytech29@gmail.com>
2023-02-06 20:52:52 +0530

    hello

--- Dictionary_functions.py
+++ Dictionary_functions.py
@@ -12,3 +12,5 @@
 # d2.update({"leena": "Coffee"})
 # print(d2.keys())
 # print(d2.items())
+empdict = {'Eno' : [1,2,3,4] , 'Ename' : ['Raj','Seema','John','Smith'],'Sal': [1000,2000,3000,4000]}
+print(empdict['Ename'][0],empdict['Sal'][0])
