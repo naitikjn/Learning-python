da1f3fc73f6fda1f5cf1ca5c05e1ff2cd783e192
naitikjn <trendytech29@gmail.com>
2023-02-15 22:05:49 +0530

    hello

--- Data types.py
+++ Data types.py
@@ -1,69 +1,69 @@
-# var1 = "Hello "
-# var2 = "Trendy tech"
-# var3 = 56
-# var4 = 14
+# # var1 = "Hello "
+# # var2 = "Trendy tech"
+# # var3 = 56
+# # var4 = 14
+# #
+# # print(str(var1) + str(var2))
+# # print(100* "hello world \n")
+# # print("enter your number")
+# # abcd = input()
+# #
+# # print("you entered", int(abcd) + 10)
+# #
+# # print("Enter the number")
+# # n1 = input()
+# # print("Enter the number")
+# # n2 = input()
+# # print("Sum of these numbers: ", int(n1) + int(n2))
+# #
+# # # Data types
+# # """
+# # 1. int()
+# # 2. float()
+# # 3. str()
 #
-# print(str(var1) + str(var2))
-# print(100* "hello world \n")
-# print("enter your number")
-# abcd = input()
+# # variables & data types
+# # fruit = "Mango"
+# # mangoLen = len(fruit)
+# # print(mangoLen)
+# # print(fruit[0:4]) # including 0 but not 4
+# # print(fruit[1:4]) # including 1 but not 4
+# # print(fruit[:5])
+# # print(fruit[0:-3])
+# # print(fruit[:len(fruit)-3])
+# # print(fruit[-1:len(fruit) - 3])
+# # print(fruit[-3:-1])
 #
-# print("you entered", int(abcd) + 10)
+# # Quick Quiz:
+# # nm = "Harry"
+# # print(nm[-4:-2])
+# # @codewithharry
 #
-# print("Enter the number")
-# n1 = input()
-# print("Enter the number")
-# n2 = input()
-# print("Sum of these numbers: ", int(n1) + int(n2))
+# # list1 = [1,2,3,4,5]
+# # str1 = 6
+# # for i in list1:
+# #     Str1 = str1 + i
+# # print(Str1)
 #
-# # Data types
-# """
-# 1. int()
-# 2. float()
-# 3. str()
-
-# variables & data types
-# fruit = "Mango"
-# mangoLen = len(fruit)
-# print(mangoLen)
-# print(fruit[0:4]) # including 0 but not 4
-# print(fruit[1:4]) # including 1 but not 4
-# print(fruit[:5])
-# print(fruit[0:-3])
-# print(fruit[:len(fruit)-3])
-# print(fruit[-1:len(fruit) - 3])
-# print(fruit[-3:-1])
-
-# Quick Quiz:
-# nm = "Harry"
-# print(nm[-4:-2])
-# @codewithharry
-
-# list1 = [1,2,3,4,5]
-# str1 = 6
-# for i in list1:
-#     Str1 = str1 + i
-# print(Str1)
-
-# i = 0
-# while (i<25):
-#     print("Python")
-#     i = i + 1
-
+# # i = 0
+# # while (i<25):
+# #     print("Python")
+# #     i = i + 1
 #
-# tup1 = (1,2,3)
-# tup = tup1*2
-# print(tup)
-# x = [1,3,6, [18]]
-# y = list(x)
-# x[3][0] =15
-# x[1] = 12
-# print(y)
-# x = 'ARIHANT'
-# for i in range(len(x)):
-#     x[i].lower()
-# print(x)
-y = str(123)
+# #
+# # tup1 = (1,2,3)
+# # tup = tup1*2
+# # print(tup)
+# # x = [1,3,6, [18]]
+# # y = list(x)
+# # x[3][0] =15
+# # x[1] = 12
+# # print(y)
+# # x = 'ARIHANT'
+# # for i in range(len(x)):
+# #     x[i].lower()
+# # print(x)
+# y = str(123)
 x = "hello" * 3
 print(x,y)
 x ="hello" + "world"
