ddce6b80e1c109f5767313751231603dbeb7fdd1
naitikjn <trendytech29@gmail.com>
2023-02-15 22:05:18 +0530

    hello

--- Lambda_Function.py
+++ Lambda_Function.py
@@ -1,17 +1,17 @@
-Lambda functions or anonymous functions
-def add(a, b):
-    return a+b
-
-# minus = lambda x, y: x-y
-
-def minus(x, y):
-    return x-y
-
-print(minus(9, 4))
-
-
-a = [[1, 14], [5, 6], [8, 23]]
-a.sort(key=lambda x: x[1])
-print(a)
-
-
+# Lambda functions or anonymous functions
+# def add(a, b):
+#     return a+b
+#
+# # minus = lambda x, y: x-y
+#
+# def minus(x, y):
+#     return x-y
+#
+# print(minus(9, 4))
+#
+#
+# a = [[1, 14], [5, 6], [8, 23]]
+# a.sort(key=lambda x: x[1])
+# print(a)
+#
+#
