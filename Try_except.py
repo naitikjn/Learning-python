daf2740a940a252bd87d5338b4331b5b161443e2
naitikjn <trendytech29@gmail.com>
2023-02-15 22:05:30 +0530

    hello

--- Try_except.py
+++ Try_except.py
@@ -1,12 +1,12 @@
-# print("Enter first number: ")
-# Num1 = int(input())
-# print("Enter second number: ")
-# Num2 = int(input())
-#
-# print("The sum of these two numbers is ",Num1+Num2)
-#
-#
-# a = 9
-# b = 8
-# c = sum((a,b))
-# print(c)
+print("Enter first number: ")
+Num1 = int(input())
+print("Enter second number: ")
+Num2 = int(input())
+
+print("The sum of these two numbers is ",Num1+Num2)
+
+
+a = 9
+b = 8
+c = sum((a,b))
+print(c)
