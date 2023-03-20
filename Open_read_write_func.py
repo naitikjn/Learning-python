5405a87039663b4124e02ff603ca264010b8c0be
naitikjn <trendytech29@gmail.com>
2023-03-20 13:11:41 +0530

    hellom

--- Open_read_write_func.py
+++ Open_read_write_func.py
@@ -1,44 +1,44 @@
-# f = open("practice.py")
-# content = f.read()
-# print(content)
-# f.close()
-#
-# f = open("practice.py")
-# content = f.read()
-# print(content)
-#
-# for line in content:
-#     print(line)
-#
-# for line in f:
-#     print(line)
-#
-# for line in f:
-#     print(line, end=""l
-# a = open("IO_Basics.py")
-# text =a.read()
-# print(text)
-# a.close()
-#
-# t = open("IO_Basics.py",'a')
-# hello = t.write("hello,guys")
-# print(t)
-#
-# t.close()
+f = open("practice.py")
+content = f.read()
+print(content)
+f.close()
 
-# f = open('myfile.txt', 'r')
-# i = 0
-# while True:
-#   i = i + 1
-#   line = f.readline()
-#   if not line:
-#     break
-#   m1 = int(line.split(",")[0])
-#   m2 = int(line.split(",")[1])
-#   m3 = int(line.split(",")[2])
-#   print(f"Marks of student {i} in Maths is: {m1*2}")
-#   print(f"Marks of student {i} in English is: {m2*2}")
-#   print(f"Marks of student {i} in SST is: {m3*2}")
+f = open("practice.py")
+content = f.read()
+print(content)
+
+for line in content:
+    print(line)
+
+for line in f:
+    print(line)
+
+for line in f:
+    print(line, end=""l
+a = open("IO_Basics.py")
+text =a.read()
+print(text)
+a.close()
+
+t = open("IO_Basics.py",'a')
+hello = t.write("hello,guys")
+print(t)
+
+t.close()
+
+f = open('myfile.txt', 'r')
+i = 0
+while True:
+  i = i + 1
+  line = f.readline()
+  if not line:
+    break
+  m1 = int(line.split(",")[0])
+  m2 = int(line.split(",")[1])
+  m3 = int(line.split(",")[2])
+  print(f"Marks of student {i} in Maths is: {m1*2}")
+  print(f"Marks of student {i} in English is: {m2*2}")
+  print(f"Marks of student {i} in SST is: {m3*2}")
 
 #   print(line)
 
