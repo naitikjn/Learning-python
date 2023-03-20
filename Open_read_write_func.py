8d780a98059342efb8dd1b1e4a724d5113f5f2ad
naitikjn <trendytech29@gmail.com>
2023-03-20 13:11:28 +0530

    hellom

--- Open_read_write_func.py
+++ Open_read_write_func.py
@@ -15,13 +15,34 @@
 #
 # for line in f:
 #     print(line, end=""l
-a = open("IO_Basics.py")
-text =a.read()
-print(text)
-a.close()
+# a = open("IO_Basics.py")
+# text =a.read()
+# print(text)
+# a.close()
+#
+# t = open("IO_Basics.py",'a')
+# hello = t.write("hello,guys")
+# print(t)
+#
+# t.close()
 
-t = open("IO_Basics.py",'a')
-hello = t.write("hello,guys")
-print(t)
+# f = open('myfile.txt', 'r')
+# i = 0
+# while True:
+#   i = i + 1
+#   line = f.readline()
+#   if not line:
+#     break
+#   m1 = int(line.split(",")[0])
+#   m2 = int(line.split(",")[1])
+#   m3 = int(line.split(",")[2])
+#   print(f"Marks of student {i} in Maths is: {m1*2}")
+#   print(f"Marks of student {i} in English is: {m2*2}")
+#   print(f"Marks of student {i} in SST is: {m3*2}")
 
-t.close()
+#   print(line)
+
+f = open('myfile2.txt', 'w')
+lines = ['line 1\n', 'line 2\n', 'line 3\n']
+f.writelines(lines)
+f.close()
