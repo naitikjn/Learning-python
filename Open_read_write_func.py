e1f716cbb57a9ef4fd6f92efad2cc6b38d44c434
naitikjn <trendytech29@gmail.com>
2023-03-23 21:45:29 +0530

    hellom

--- Open_read_write_func.py
+++ Open_read_write_func.py
@@ -1,29 +1,29 @@
-# f = open("practice.py")
-# content = f.read()
-# print(content)
-# f.close()
-#
-# f = open("practice.py")
-# content = f.read()
-# print(content)
-#
-# for line in content:
-#     print(line)
-#
-# for line in f:
-#     print(line)
-#
-# for line in f:
-#     print(line, end=""l
-# a = open("IO_Basics.py")
-# text =a.read()
-# print(text)
-# a.close()
-#
-# t = open("IO_Basics.py",'a')
-# hello = t.write("hello,guys")
-# print(t)
-#
+f = open("practice.py")
+content = f.read()
+print(content)
+f.close()
+
+f = open("practice.py")
+content = f.read()
+print(content)
+
+for line in content:
+    print(line)
+
+for line in f:
+    print(line)
+
+for line in f:
+    print(line, end=""l
+a = open("IO_Basics.py")
+text =a.read()
+print(text)
+a.close()
+
+t = open("IO_Basics.py",'a')
+hello = t.write("hello,guys")
+print(t)
+
 # t.close()
 #
 # f = open('myfile.txt', 'r')
