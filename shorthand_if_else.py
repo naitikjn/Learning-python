50b2dd0e78e22cc336b15a69dae927cfd457577d
naitikjn <trendytech29@gmail.com>
2023-03-23 21:45:43 +0530

    hellom

--- shorthand_if_else.py
+++ shorthand_if_else.py
@@ -1,16 +1,16 @@
-# # If else - elif condition
-# a = int(input("enter num a :\n"))
-# b = int(input("enter num b :\n"))
-#
-# if a<b: print("b is greator than a")
-# elif a==b: print("same hai")
-#
-#
-# num1 = input("1")
-# num2 = input("2")
-# sum(num1+num2)
-#
-#
-#
-# print(sum)
-#
+# If else - elif condition
+a = int(input("enter num a :\n"))
+b = int(input("enter num b :\n"))
+
+if a<b: print("b is greator than a")
+elif a==b: print("same hai")
+
+
+num1 = input("1")
+num2 = input("2")
+sum(num1+num2)
+
+
+
+print(sum)
+
