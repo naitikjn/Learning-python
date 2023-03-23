f903cd184ab714fa1884d423092d79925f829dba
naitikjn <trendytech29@gmail.com>
2023-03-23 21:46:16 +0530

    hellom

--- Functions.py
+++ Functions.py
@@ -11,33 +11,33 @@
 #
 # function1()
 # function1()
-# function1()
-# function1()
-#
-# def function2(a,b):
-#     average =(a+b)/2
-#     print(average)
-# function2(56,67)
-#
-#
-# num1 = input("1")
-# print(num1)
-#
-# a = 10
-# b = 10
-# gmean = (a*b)/(a+b)
-# print(gmean)
-#
-# def isgreator(a,b):
-#     if (a>b):
-#         print("A is greator")
-#     else:
-#         print("B is greator")
-#
-#
+function1()
+function1()
 
-# a = 10
-# b = 10
+def function2(a,b):
+    average =(a+b)/2
+    print(average)
+function2(56,67)
+
+
+num1 = input("1")
+print(num1)
+
+a = 10
+b = 10
+gmean = (a*b)/(a+b)
+print(gmean)
+
+def isgreator(a,b):
+    if (a>b):
+        print("A is greator")
+    else:
+        print("B is greator")
+
+
+
+a = 10
+b = 10
 # gmean = (a*b)/(a+b)
 # print(gmean)
 
