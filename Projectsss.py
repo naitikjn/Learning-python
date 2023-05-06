b4b8fc716a2d439a346787d83d275922aec4b9f8
naitikjn <trendytech29@gmail.com>
2023-05-06 20:54:36 +0530

    hellom

--- Projectsss.py
+++ Projectsss.py
@@ -1,8 +1,8 @@
 # first project Calculator
 print("***********CALCULATOR************")
 
-num1 = float(input("enter 1:"))
-num2 = float(input("enter 2:"))
+num1 = float(input("enter num1:"))
+num2 = float(input("enter num2:"))
 print("------------ANSWER------------")
 
 print("Addition is :",num1 +num2)
@@ -10,5 +10,5 @@ print("Subtraction is :",num1 - num2)
 print("Multiplication is :",num1 *num2)
 print("Division is :",num1 /num2)
 print("Exponent & Power is :",num1 **num2)
-print("Addition is :",num1 +num2)
+print("Floor Division is :",num1 //num2)
 print("Remainder module is :",num1%num2)
