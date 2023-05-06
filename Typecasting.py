f46b7297df00d0fd9b20f60871994ded73d71791
naitikjn <trendytech29@gmail.com>
2023-05-06 20:54:43 +0530

    hellom

---
+++ Typecasting.py
@@ -0,0 +1,11 @@
+#Typecasting
+
+a = "1"
+b = '2'
+print(a)
+print(b)
+print(int(a) + int(b))
+
+#Taking Input as User
+name = input("Enter your name :")
+print("My Name is",name,"I am cool and Handsome.")
