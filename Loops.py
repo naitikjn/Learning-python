ed7e4e12b89390b63ecb6777d0064185155833a6
naitikjn <trendytech29@gmail.com>
2023-05-16 07:44:55 +0530

    hellom

--- Loops.py
+++ Loops.py
@@ -1,95 +1,95 @@
-# list1 = ['harry', 1],['larry', 2],['karan', 3],['prakash', 4]
-# #print(list1[0],list1[1])
-# for item , lollypop in list1:
-#     print(item,"and lolly is", lollypop)
+# # list1 = ['harry', 1],['larry', 2],['karan', 3],['prakash', 4]
+# # #print(list1[0],list1[1])
+# # for item , lollypop in list1:
+# #     print(item,"and lolly is", lollypop)
+# #
+# #
+# #     var1 = "Hell" \
+# #            "o "
+# #     var2 = "Trendy tech"
+# # #types python
+# # print(str(var1) + str(var2))
+# # print(100* "hello world \n")
+# # print("enter your number")
+# # abcd = input()
+# #
+# # print("you entered", int(abcd) + 1)
+# #
+# # # While loops in python
+# # i = 0
+# # while(i<45):
+# #     print(i)
+# #
+# #     i = i+1
+# #
+# # i = 10
+# # while(i<50):
+# #     print(i)
+# #     i =i +9
+# #
+# # i = 1
+# # while(i<50):
+# #     i =i +1
+# #
+# # list1 = ['harry', 1], ['larry', 2], ['smith', 3]
+# # for item , lollypop in list1:
+# #     print(item,"and mohan is", lollypop)
+# #     None
+#
+# # loops in string for loop
+#
+# # name = "naitik"
+# # for i in name:
+# #     print(i)
+# #     if(i=='a'):
+# #         print("This is something special")
+# #     else:
+# #         print("nops!")
+#
+# # for loops in list
+# # colours = ['red','blue','green','yellow','pink']
+# # i = 0
+# # for i in colours:
+# #     print(i)
+#
+# # use of range function
+# # name = 'naitik'
+# # for i in range(0,20001):
+# #     print(i)
+#
+# # Print table of any number
+# # num = float(input("Enter the number: "))
+# # i = 0
+# # for i in range(1,11):
+# #     print(num,'x',i,'=',i*num)
+#
+# # while loops in python
+#
+# i =0
+# # while(i<3):
+# #     print("hello",i)
+# #     i = i + 1
+# # print("done with the loop" )
 #
 #
-#     var1 = "Hell" \
-#            "o "
-#     var2 = "Trendy tech"
-# #types python
-# print(str(var1) + str(var2))
-# print(100* "hello world \n")
-# print("enter your number")
-# abcd = input()
+# # Break statements in loop
+# # for i in range(0,15):
+# #     print("Hello world!",i)
+# #     if (i==10):
+# #
+# #         break
+# # print("Done with the loop")
 #
-# print("you entered", int(abcd) + 1)
-#
-# # While loops in python
-# i = 0
-# while(i<45):
-#     print(i)
-#
-#     i = i+1
-#
-# i = 10
-# while(i<50):
-#     print(i)
-#     i =i +9
-#
-# i = 1
-# while(i<50):
-#     i =i +1
-#
-# list1 = ['harry', 1], ['larry', 2], ['smith', 3]
-# for item , lollypop in list1:
-#     print(item,"and mohan is", lollypop)
-#     None
-
-# loops in string for loop
-
-# name = "naitik"
-# for i in name:
-#     print(i)
-#     if(i=='a'):
-#         print("This is something special")
-#     else:
-#         print("nops!")
-
-# for loops in list
-# colours = ['red','blue','green','yellow','pink']
-# i = 0
-# for i in colours:
-#     print(i)
-
-# use of range function
-# name = 'naitik'
-# for i in range(0,20001):
-#     print(i)
-
-# Print table of any number
-# num = float(input("Enter the number: "))
-# i = 0
-# for i in range(1,11):
-#     print(num,'x',i,'=',i*num)
-
-# while loops in python
-
-i =0
-# while(i<3):
-#     print("hello",i)
-#     i = i + 1
-# print("done with the loop" )
-
-
-# Break statements in loop
-# for i in range(0,15):
-#     print("Hello world!",i)
-#     if (i==10):
-#
-#         break
-# print("Done with the loop")
-
-# i = 1
-# while True:
-#     print(i)
-#     i = i + 1
-#
-#     if (i==30):
-#         print("Loop is over")
-#         break
-#
-#
+# # i = 1
+# # while True:
+# #     print(i)
+# #     i = i + 1
+# #
+# #     if (i==30):
+# #         print("Loop is over")
+# #         break
+# #
+# #
 # for i in range(0,101,2,):
 #     print("HELLO",i)
 # print("*********** FOR LOOPS ************".center(50))
@@ -101,6 +101,12 @@ i =0
 #
 # Name = name.split()
 # print(Name)
+#
+# for i in range(1,10000):
+#  print(i)
+#
 
-for i in range(1,10000):
- print(i)
+i=  0
+while(i<5):
+    print(i,"HELLO".center(50))
+    i= i + 1
