5936490404b7dd1ad5a9f06863b41707bc9fb166
naitikjn <trendytech29@gmail.com>
2023-05-22 08:00:36 +0530

    hellom

--- Tuples.py
+++ Tuples.py
@@ -1,20 +1,34 @@
-tup = [1,2,76,89,128]
-tup[0] = 90
-print(type(tup),tup)
-print(len(tup))
-print(tup[0])
-print(tup[1])
-print(tup[2])
-print(tup[3])
-print(tup[4])
-i = int(input("Enter the number: "))
-if i in tup:
-    print("YES it is in tup")
-else:
-    print("NO")
-tup2 = tup[1:4]
+# tup = [1,2,76,89,128]
+# tup[0] = 90
+# print(type(tup),tup)
+# print(len(tup))
+# print(tup[0])
+# print(tup[1])
+# print(tup[2])
+# print(tup[3])
+# print(tup[4])
+# i = int(input("Enter the number: "))
+# if i in tup:
+#     print("YES it is in tup")
+# else:
+#     print("NO")
+# tup2 = tup[1:4]
+# print(tup2)
+# tup.extend(tup2)
+# print(tupbb)
+#
+#
+tup1 = (1,2,3)
+print(tup1)
+tup2 = (4,5,6)
 print(tup2)
-tup.extend(tup2)
-print(tupbb)
-
-
+print(tup1.index(2))
+tup2 = tup1
+# while True:
+#     print("yes")
+# else:
+#     print("no")
+#
+print(tup2)
+print(tup2.__len__())
+print(tup2.count(2))
