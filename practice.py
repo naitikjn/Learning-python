207f371f6fe8217eb4050b3deda50d086ccec3ee
naitikjn <trendytech29@gmail.com>
2023-05-25 07:33:30 +0530

    hellom

--- practice.py
+++ practice.py
@@ -24,28 +24,160 @@
 # else:
 #     print(secret.reverse())
 #
-import requests
+# import requests
+#
+#
+# def get_weather(api_key, location):
+#     url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
+#     response = requests.get(url)
+#     data = response.json()
+#
+#     if data["cod"] == "404":
+#         print("Invalid location. Please try again.")
+#         return
+#
+#     city = data["name"]
+#     temperature = data["main"]["temp"]
+#     weather_desc = data["weather"][0]["description"]
+#
+#     print(f"Weather in {city}:")
+#     print(f"Temperature: {temperature} K")
+#     print(f"Description: {weather_desc}")
+#
+#
+# # Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
+# api_key = 'YOUR_API_KEY'
+# location = input("Enter a city name: ")
+# get_weather(api_key, location)
+
+print("Kaun Banega Crorepati".center(50))
+questions = [
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+    [
+        "Which language was used to create fb?", "Python", "French", "JavaScript",
+        "Php", "None", 4
+    ],
+]
+
+levels = [1000, 2000, 3000, 5000, 10000, 20000, 40000, 80000, 160000, 320000]
+money = 0
+for i in range(0, len(questions)):
+
+    question = questions[i]
+    print(f"\n\nQuestion for Rs. {levels[i]}")
+    print(f"a. {question[1]}          b. {question[2]} ")
+    print(f"c. {question[3]}          d. {question[4]} ")
+    reply = int(input("Enter your answer (1-4) or  0 to quit:\n"))
+    if (reply == 0):
+        money = levels[i - 1]
+        break
+    if (reply == question[-1]):
+        print(f"Correct answer, you have won Rs. {levels[i]}")
+        if (i == 4):
+            money = 10000
+        elif (i == 9):
+            money = 320000
+        elif (i == 14):
+            money = 10000000
+    else:
+        print("Wrong answer!")
+        break
+
+print(f"Your take home money is {money}")
 
 
-def get_weather(api_key, location):
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}"
-    response = requests.get(url)
-    data = response.json()
-
-    if data["cod"] == "404":
-        print("Invalid location. Please try again.")
-        return
-
-    city = data["name"]
-    temperature = data["main"]["temp"]
-    weather_desc = data["weather"][0]["description"]
-
-    print(f"Weather in {city}:")
-    print(f"Temperature: {temperature} K")
-    print(f"Description: {weather_desc}")
 
 
-# Replace 'YOUR_API_KEY' with your OpenWeatherMap API key
-api_key = 'YOUR_API_KEY'
-location = input("Enter a city name: ")
-get_weather(api_key, location)
+
+
