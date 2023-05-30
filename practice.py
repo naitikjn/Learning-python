c58d29e5079a4a962ae676d9bba38561dbba34fe
naitikjn <trendytech29@gmail.com>
2023-05-30 14:07:43 +0530

    hellom

--- practice.py
+++ practice.py
@@ -163,15 +163,15 @@
 # #         money = levels[i - 1]
 # #         break
 # #     if (reply == question[-1]):
-# #         print(f"Correct answer, you have won Rs. {levels[i]}")
-# #         if (i == 4):
-# #             money = 10000
-# #         elif (i == 9):
-# #             money = 320000
-# #         elif (i == 14):
-# #             money = 10000000
-# #     else:
-# #         print("Wrong answer!")
-# #         break
-# #
-# # print(f"Your take home money is {money}")
+        print(f"Correct answer, you have won Rs. {levels[i]}")
+        if (i == 4):
+            money = 10000
+        elif (i == 9):
+            money = 320000
+        elif (i == 14):
+            money = 10000000
+    else:
+        print("Wrong answer!")
+        break
+
+print(f"Your take home money is {money}")
